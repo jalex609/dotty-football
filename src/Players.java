5e8f0aa890f42cacc6a89d924fb1b623935fa3a8
jmanlikescake <jalex609@gmail.com>
2017-06-22 20:48:32 -0400

    Added kicking field goals, punting

--- src/Players.java
+++ src/Players.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import tester.*;
 //add kicking, fix extra distance bug, clean up code and test and find more bugs
 
-
 // class representing the football field itself
 class Field extends World {
   ArrayList<ArrayList<Cell>> board;
@@ -184,16 +183,20 @@ class Field extends World {
     }
   }
 
-  void resetAfterTD() {
+  void resetAfterAction(int fieldPosition, boolean overhalf, int whatScore) {
     if (homepossesion) {
       this.resetAwayPosition();
-      this.info.fieldposition = 20;
-      this.info.overhalfway = false;
+      this.score.home += whatScore;
+      this.info.down = 1;
+      this.info.fieldposition = fieldPosition;
+      this.info.overhalfway = overhalf;
     }
     else {
       this.resetHomePosition();
-      this.info.fieldposition = 20;
-      this.info.overhalfway = false;
+      this.score.away += whatScore;
+      this.info.down = 1;
+      this.info.fieldposition = fieldPosition;
+      this.info.overhalfway = overhalf;
     }
   }
 
@@ -252,21 +255,63 @@ class Field extends World {
       this.firstMove = true;
       player.position = player.position.up;
     }
-
     else if (key.equals("down")) {
       this.score.time -= 1;
       this.firstMove = true;
       player.position = player.position.down;
     }
+    else if (key.equals("p")) {
+      if (this.info.down == 4 && !this.firstMove) {
+        if (this.info.fieldposition + 40 > 50 && !this.info.overhalfway) {
+          this.resetAfterAction(100 - (this.info.fieldposition + 40), false, 0);
+        }
+        else {
+          this.resetAfterAction(this.info.fieldposition + 40, true, 0);
+        }
+      }
+    }
+    else if (key.equals("k")) {
+      if (this.info.overhalfway && !this.firstMove) {
+        double rand = Math.random();
+        if (this.info.fieldposition > 40) {
+          if (rand > .9) {
+            this.resetAfterAction(20, false, 3);
+          }
+        }
+        else if (this.info.fieldposition > 30 && this.info.fieldposition < 40) {
+          if (rand > .7) {
+            this.resetAfterAction(20, false, 3);
+          }
+        }
+        else if (this.info.fieldposition > 20 && this.info.fieldposition < 30) {
+          if (rand > .5) {
+            this.resetAfterAction(20, false, 3);
+          }
+        }
+        else if (this.info.fieldposition > 10 && this.info.fieldposition < 20) {
+          if (rand > .3) {
+            this.resetAfterAction(20, false, 3);
+          }
+        }
+        else if (this.info.fieldposition > 0 && this.info.fieldposition < 10) {
+          if (rand > .1) {
+            this.resetAfterAction(20, false, 3);
+          }
+        }
+        else {
+          this.resetAfterAction(20, false, 0);
+        }
+      }
+    }
 
     if (info.fieldposition == 0) {
       if (homepossesion) {
         this.score.home += 7;
-        this.resetAfterTD();
+        this.resetAfterAction(20, false, 7);
       }
       else {
         this.score.away += 7;
-        this.resetAfterTD();
+        this.resetAfterAction(20, false, 7);
       }
     }
   }
@@ -292,9 +337,8 @@ class Field extends World {
 
     result.placeImageXY(new RectangleImage(4 * Field.IMG_SCALING, Field.IMG_SCALING,
         OutlineMode.SOLID, Color.BLACK), 0, 350);
-    result.placeImageXY(
-        new RectangleImage(2 * Field.IMG_SCALING, Field.IMG_SCALING, OutlineMode.SOLID, Color.BLACK), 800,
-        350);
+    result.placeImageXY(new RectangleImage(2 * Field.IMG_SCALING, Field.IMG_SCALING,
+        OutlineMode.SOLID, Color.BLACK), 800, 350);
     result.placeImageXY(this.info.render(), 650, 350);
     result.placeImageXY(this.score.render(), 350, 350);
     return result;
